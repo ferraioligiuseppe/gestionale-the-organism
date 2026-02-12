736ceb5517a66725d7b667c09247c6683383da21
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-02-12 15:04:31 +0100

    Update app.py

--- vision_manager/app.py
+++ vision_manager/app.py
@@ -11,42 +11,6 @@ from ui_prescrizione import ui_prescrizione
 from ui_storico_confronto import ui_storico_confronto
 from ui_diagnostica import ui_diagnostica
 import streamlit as st
-import streamlit_authenticator as stauth
-
-def require_login():
-    cookie = st.secrets["auth"]["cookie"]
-
-    u = st.secrets["auth_temp"]["username"]
-    n = st.secrets["auth_temp"]["name"]
-    pw_plain = st.secrets["auth_temp"]["password_plain"]
-
-    # HASH al volo (temporaneo)
-    pw_hash = stauth.Hasher([pw_plain]).generate()[0]
-
-    credentials = {"usernames": {u: {"name": n, "password": pw_hash}}}
-
-    authenticator = stauth.Authenticate(
-        credentials,
-        cookie["name"],
-        cookie["key"],
-        cookie["expiry_days"],
-    )
-
-    name, auth_status, username = authenticator.login("Login", "main")
-
-    if auth_status is True:
-        authenticator.logout("Logout", "sidebar")
-        st.session_state["auth_user"] = username
-        return
-
-    if auth_status is False:
-        st.error("Username o password non corretti")
-        st.stop()
-
-    st.info("Inserisci username e password")
-    st.stop()
-
-require_login()
 
 st.set_page_config(page_title="The Organism – Vision Manager", layout="wide")
 
