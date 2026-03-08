53a064ffeb7cd44f1f53c9d59d6bf0944de3c149
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-03-08 10:45:58 +0100

    Delete pdf_privacy.py

--- pdf_privacy.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Wrapper sicuri per area PNEV / questionari."""
-from .ui_pnev import render_pnev_section
-from .pnev_questionari import render_public_questionario_if_needed
-from .pnev_summary import collect_inpps_ui
-__all__ = ["render_pnev_section", "render_public_questionario_if_needed", "collect_inpps_ui"]
