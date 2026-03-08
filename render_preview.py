4441be8b6ec4f34317078a07b9106973eeb6ada8
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-03-08 10:52:08 +0100

    Delete render_preview.py

--- render_preview.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Compat layer per future estrazioni PDF referto.
-
-In questa fase non sposta logica critica: espone un punto di aggancio stabile.
-"""
-
-def get_pdf_referto_backend():
-    return "app_core"
