63645b8b68f0e7c0d79d4e7a3997f41e9626ba8f
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-03-15 20:01:59 +0100

    Delete privacy directory

--- privacy/sign_page.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Entry point modulare per la pagina pubblica firma online."""
-
-def render_public_sign_page(*args, **kwargs):
-    from app_core import ui_public_sign_page
-    return ui_public_sign_page(*args, **kwargs)
--- privacy/ui_privacy.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Shim compatibile: usa il nuovo entry point canonico."""
-from .ui_privacy_section import render_privacy_section
