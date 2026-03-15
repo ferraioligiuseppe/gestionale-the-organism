bf20b59a64bcd99b7bc5a3d0f6a409d9ff04d046
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-03-15 20:01:47 +0100

    Delete pnev directory

--- pnev/pnev_engine.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Shim compatibile: usa il nuovo entry point canonico."""
-from .ui_pnev import render_pnev_section
--- pnev/ui_pnev.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Entry point modulare canonico per area PNEV / questionari."""
-
-def render_pnev_section(*args, **kwargs):
-    from app_core import ui_anamnesi
-    return ui_anamnesi(*args, **kwargs)
