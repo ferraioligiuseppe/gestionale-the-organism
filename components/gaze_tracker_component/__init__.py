dfce2bc16ddee533b5f241a5001f00c0dded8147
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-03-19 16:44:44 +0100

    Add files via upload

--- components/gaze_tracker_component/__init__.py
+++ components/gaze_tracker_component/__init__.py
@@ -1,16 +1,7 @@
-import os
 import streamlit.components.v1 as components
 
-_component_func = components.declare_component(
-    "gaze_tracker_component",
-    path=os.path.join(os.path.dirname(__file__), "frontend"),
-)
 
-def gaze_tracker_component(*, key=None, mode="calibration", task=None, height=760):
-    return _component_func(
-        key=key,
-        mode=mode,
-        task=task or {},
-        default={},
-        height=height,
-    )
+gaze_tracker_component = components.declare_component(
+    "gaze_tracker_component",
+    path="components/gaze_tracker_component/frontend",
+)
