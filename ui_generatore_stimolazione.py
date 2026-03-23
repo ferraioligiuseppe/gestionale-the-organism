212eb52cc937b681755a5f274632a08096a49ac8
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-03-23 17:10:11 +0100

    Delete ui_generatore_stimolazione.py

--- ui_generatore_stimolazione.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Compatibility shim: usa sempre il modulo unico in modules/stimolazione_uditiva/."""
-from modules.stimolazione_uditiva.ui_generatore_stimolazione import *  # noqa: F401, F403
