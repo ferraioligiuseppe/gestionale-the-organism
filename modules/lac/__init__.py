4cb31e947bf0f78b5c1c3e25e6b741edfc32461d
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-03-29 13:09:24 +0200

    Add files via upload

---
+++ modules/lac/__init__.py
@@ -0,0 +1,10 @@
+from .lac_engine import (
+    ESA_DATA,
+    esa_lookup,
+    toffoli_calc,
+    hyperopia_calc,
+    astig_calc,
+    presbyopia_calc,
+    estimate_clearance,
+)
+from .lac_decision import build_curves
