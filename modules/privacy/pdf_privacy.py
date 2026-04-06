f1fa767484bb3e466da021287f61239b54620c75
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-04-06 21:12:25 +0200

    fix: import modules.app_core

--- modules/privacy/pdf_privacy.py
+++ modules/privacy/pdf_privacy.py
@@ -5,7 +5,7 @@ Non sposta ancora la logica dal core: reindirizza in modo sicuro.
 """
 
 def privacy_templates_info():
-    from app_core import (
+    from modules.app_core import (
         PDF_PRIVACY_ADULTO_TEMPLATE,
         PDF_PRIVACY_MINORE_TEMPLATE,
         PDF_PRIVACY_ADULTO_SIGN_TEMPLATE,
@@ -19,9 +19,9 @@ def privacy_templates_info():
     }
 
 def resolve_privacy_path(path: str) -> str:
-    from app_core import _privacy_abs_path
+    from modules.app_core import _privacy_abs_path
     return _privacy_abs_path(path)
 
 def check_privacy_templates_ui():
-    from app_core import _check_privacy_templates_ui
+    from modules.app_core import _check_privacy_templates_ui
     return _check_privacy_templates_ui()
