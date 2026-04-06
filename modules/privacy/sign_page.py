b05482dca32174e5ffc3e3e5fac1cc552050dadc
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-04-06 21:12:27 +0200

    fix: import modules.app_core

--- modules/privacy/sign_page.py
+++ modules/privacy/sign_page.py
@@ -2,5 +2,5 @@
 """Entry point modulare per la pagina pubblica firma online."""
 
 def render_public_sign_page(*args, **kwargs):
-    from app_core import ui_public_sign_page
+    from modules.app_core import ui_public_sign_page
     return ui_public_sign_page(*args, **kwargs)
