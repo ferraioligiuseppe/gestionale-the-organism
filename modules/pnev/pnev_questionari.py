2854572285af17e03364ed24a26dd7a2dd624ffd
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-04-06 21:12:20 +0200

    fix: import modules.app_core

--- modules/pnev/pnev_questionari.py
+++ modules/pnev/pnev_questionari.py
@@ -2,5 +2,5 @@
 """Wrapper per questionari pubblici e link tokenizzati."""
 
 def render_public_questionario_if_needed(*args, **kwargs):
-    from app_core import maybe_handle_public_questionario
+    from modules.app_core import maybe_handle_public_questionario
     return maybe_handle_public_questionario(*args, **kwargs)
