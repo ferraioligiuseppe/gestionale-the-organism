1234566aeb89e97b08842c2a4c396148c9486b57
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-04-06 21:12:23 +0200

    fix: import modules.app_core

--- modules/pnev/pnev_summary.py
+++ modules/pnev/pnev_summary.py
@@ -2,5 +2,5 @@
 """Wrapper per raccolta / sintesi questionari INPPS."""
 
 def collect_inpps_ui(*args, **kwargs):
-    from app_core import inpps_collect_ui
+    from modules.app_core import inpps_collect_ui
     return inpps_collect_ui(*args, **kwargs)
