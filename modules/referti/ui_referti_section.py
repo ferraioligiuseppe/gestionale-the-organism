dcdadffa38cc214bc0591c54798fd2f2117e2bcd
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-04-07 22:33:48 +0200

    Add files via upload

--- modules/referti/ui_referti_section.py
+++ modules/referti/ui_referti_section.py
@@ -2,5 +2,5 @@
 """Entry point modulare canonico per referti / relazioni cliniche."""
 
 def render_referti_section(*args, **kwargs):
-    from app_core import ui_relazioni_cliniche
+    from modules.app_core import ui_relazioni_cliniche
     return ui_relazioni_cliniche(*args, **kwargs)
