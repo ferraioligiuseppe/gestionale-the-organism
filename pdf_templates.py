3e582dc1ae1a068e666dc8bba501fc05053be8da
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-04-20 19:04:39 +0200

    Delete pdf_templates.py

--- pdf_templates.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Compatibility shim: importa dal modulo canonico in modules/."""
-from modules.pdf_templates import (  # noqa: F401
-    make_overlay_pdf,
-    merge_overlay_on_template,
-    build_pdf,
-)
