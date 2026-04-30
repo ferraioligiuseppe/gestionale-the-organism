bad26a5301e3a92af03922db1d54b18c5c673476
GitHub Action - Ripulitura <actions@github.com>
2026-04-30 20:19:39 +0000

    🧹 Ripulitura: rimossi duplicati, file old, _archive, vision_manager orfano
    Eseguito automaticamente via GitHub Action.
    - Eliminata cartella _archive/
    - Eliminati file *_old, *_patched, *_backup, *_pre_variazione, *_CORRETTO
    - Eliminati README duplicati (mantenuto solo README.md)
    - Eliminate copie duplicate di app_core.py nella root
    - Eliminata cartella vision_manager/ orfana (asset salvati in assets/_legacy_vision_manager/)
    - Eliminati __pycache__ e file temporanei

--- vision_manager/ui_generatore_stimolazione.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Compat shim: usa sempre il modulo unico in modules/stimolazione_uditiva/."""
-
-from modules.stimolazione_uditiva.ui_generatore_stimolazione import ui_generatore_stimolazione
-
-__all__ = ["ui_generatore_stimolazione"]
