16a5ea2086f23bc218c33037bb106b547c9e9922
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-05-03 16:23:29 +0200

    Delete modules/modules/consensi_costellazioni directory

--- modules/modules/consensi_costellazioni/seeders/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Seeders per i template di consenso costellazioni."""
-
-from .costellazioni import seed_template, seed_hook, TUTTI_I_TEMPLATE
-
-__all__ = ["seed_template", "seed_hook", "TUTTI_I_TEMPLATE"]
