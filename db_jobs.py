2a75c5adfa43918dda62735cc94a077936aed9f6
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-08-01 19:01:36 +0200

    Delete db_jobs.py

--- db_jobs.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Compatibility shim: importa dal modulo canonico in modules/."""
-from modules.db_jobs import (  # noqa: F401
-    ensure_jobs_schema,
-    seed_tomatis_presets,
-)
