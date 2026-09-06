6e127d1862d8e182f6078995e7af89f59e58e6fe
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-09-06 11:44:03 +0200

    Delete app_udito_router.py

--- app_udito_router.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Compatibility shim: il codice attivo è in modules/app_udito_router.py."""
-from modules.app_udito_router import *  # noqa: F401, F403
-from modules.app_udito_router import dispatch_udito_section
