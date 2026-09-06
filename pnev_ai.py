04617f346230631c0a7997fe22905c3bf887410b
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-09-06 11:44:36 +0200

    Delete pnev_ai.py

--- pnev_ai.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Compatibility shim: importa dal modulo canonico in modules/."""
-from modules.pnev_ai import (  # noqa: F401
-    generate_hypothesis,
-    generate_plan,
-    apply_to_session,
-)
