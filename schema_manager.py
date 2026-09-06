6153c39f41099a69fac16fe9b86dd7aa0533f7cb
ferraioligiuseppe <dr.ferraioligiuseppe@gmail.com>
2026-09-06 11:43:17 +0200

    Delete schema_manager.py

--- schema_manager.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Compatibility shim: importa dal modulo canonico in modules/."""
-from modules.schema_manager import (  # noqa: F401
-    ensure_all_schemas,
-    ensure_auth_schema,
-    ensure_core_schema,
-    ensure_vision_schema,
-    ensure_osteo_schema,
-)
